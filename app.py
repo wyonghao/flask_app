a9479d5c3e4d234ab63ce06808af82927bb4116e
leo <wyonghao@outlook.com>
2023-08-13 23:27:38 +0100

    add auth debug and load_dotenv
     load_dotenv needs to be explicitly called when use gunicorn,

--- app.py
+++ app.py
@@ -1,30 +1,44 @@
 import os
-
 import openai
 from flask import Flask, redirect, render_template, request, url_for, Response
 from functools import wraps
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+load_dotenv() # load environment variables from .env file for gunicorn to use
 
 # load your username and password here
 USERNAME = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
 
+print(f"Loaded USERNAME from env: '{USERNAME}'")
+print(f"Loaded PASSWORD from env: '{PASSWORD}'")
+
 def check_auth(username, password):
     """This function is called to check if a username/password combination is valid."""
-    return username == USERNAME and password == PASSWORD
+    is_authenticated = username == USERNAME and password == PASSWORD
+    print(f"Checking auth for {username}:{password}")
+    print(f"Authentication result: {is_authenticated}")
+    return is_authenticated
 
 def authenticate():
     """Sends a 401 response that enables basic auth."""
-    return Response('You need to login.', 401, {'WWW-Authenticate': 'Basic realm="Login!"'})
+    resp = Response('You need to login.', 401)
+    resp.headers['WWW-Authenticate'] = 'Basic realm="Login!"'
+    return resp
 
 def requires_auth(f):
     """A decorator to ensure the user is logged in."""
     @wraps(f)
     def decorated(*args, **kwargs):
         auth = request.authorization
-        if not auth or not check_auth(auth.username, auth.password):
+        if not auth:
+            print("No authorization provided.")
+            return authenticate()
+        if not check_auth(auth.username, auth.password):
+            print("Authentication failed.")
             return authenticate()
         return f(*args, **kwargs)
     return decorated
@@ -40,11 +54,17 @@ def index():
             temperature=0.6,
             max_tokens=250,
         )
-        return redirect(url_for("index", result=response.choices[0].text))
+        # Print statement before the redirect
+        print("About to redirect...")
+
+        result_redirect = redirect(url_for("index", result=response.choices[0].text))
+        
+        # Print statement after the redirect
+        print("Redirected with result:", response.choices[0].text)
+        return result_redirect
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
-
 
 def generate_prompt(question):
     return """Answer the following question:
